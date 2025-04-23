2f1d5f728c0c5f8bd2b8dbdd47f39b87ccf506e7
sk233 <9653231+shike233@user.noreply.gitee.com>
2025-04-23 13:58:50 +0800

    调整错误密码提示信息的位置

--- ArduinoLock.ino
+++ ArduinoLock.ino
@@ -87,8 +87,8 @@ void loop()
                 Serial.println("Admin password is incorrect!"); //! 密码错误
                 beep(3);                                        // 蜂鸣器响
                 u8g2.clearBuffer();
-                u8g2.drawXBMP(73, 18, 16, 16, str39); // 密
-                u8g2.drawXBMP(89, 18, 16, 16, str40); // 码
+                u8g2.drawXBMP(25, 20, 16, 16, str39); // 密
+                u8g2.drawXBMP(41, 20, 16, 16, str40); // 码
                 u8g2.drawXBMP(57, 20, 16, 16, str5);  // 错
                 u8g2.drawXBMP(73, 20, 16, 16, str6);  // 误
                 u8g2.sendBuffer();                    // 开显示
